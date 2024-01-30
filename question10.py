032646f8ebee9c6734b9381c7af9961da214a024
SAI NIKHIL <46823829+Kc212692@users.noreply.github.com>
2024-01-29 23:16:57 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,32 +1,47 @@
 def question10_1():
-    """
-    answer: python bool (True or False)
-    """
+    answer=True 
+    # It doesn't add anything if we deal with the count
     return answer
 
 def question10_2():
+    answer=True     # The correlation value between 2 real values vary between -1(strong -ve correlation) and 1 (strong +ve correlation)
     return answer
 
 def question10_3():
+    answer=False    # Sampling might lead to information loss which is important in the case of time series. 
     return answer
 
 def question10_4():
+    answer=True     # Certainly all the outliers are not always noise, but in few cases
     return answer
 
 def question10_5():
+    answer=False     # In a high level, noise is very inappropriate data, whereas outliers are those points that 
+    # deviate from the original data trend (pattern)
     return answer
 
 def question10_6():
-    return answer
+    answer=False     return answer 
 
 def question10_7():
-    return answer
+    answer=True # g
+    # well if cosine measure is 1, that indicates the 2 vectors are parallel to each other, which means
+    # both are identical to each other.
+    return answer 
 
 def question10_8():
-    return answer
+    answer=False # h
+    # Its not always true, there are cases for example, if a person own 10(but not 10.5) cars, 
+    #it can be discrete and ratio (can own 0 cars)
+    return answer  
 
 def question10_9():
-    return answer
+    answer=False  # i
+    # Yes quantitative variables talk about the quantity, but not necessarily the quantity needs to be continuous.
+    # cases like 10 kg, 100 meters often come under discrete, but not continuous.
+    return answer   
 
 def question10_10():
-    return answer
+    answer=False # j
+    # not necessarily, because we will lose the order associated between the values. 
+    return answer
